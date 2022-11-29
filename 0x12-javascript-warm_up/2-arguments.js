f19672f0fe250824a00acfa3931b31a4f08ef6e1
kwamewilder <oheneba.ega@outlook.com>
2022-11-29 15:06:14 -0800

    arguments:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
-} else {
-	console.log('Arguments found');
-}
+const argc = process.argv.length;
 
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+} else {
+  console.log('No argument');
+}
