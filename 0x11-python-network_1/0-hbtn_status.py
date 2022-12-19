b7e1db364a03dc2d5e8696937a2da724a5d27d1a
kwamewilder <oheneba.ega@outlook.com>
2022-12-19 12:20:59 -0800

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-""" This module fetches https://alx-intranet.hbtn.io/status
 """
-
-import urllib.request
-
-
-def get_alx_status():
-    """ This function fetches https://alx-intranet.hbtn.io/status
-    """
-    url = "https://alx-intranet.hbtn.io/status"
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as resp:
-        html = resp.read()
-        print("Body response:")
+fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
-
-
-if __name__ == "__main__":
-    get_alx_status()
-
